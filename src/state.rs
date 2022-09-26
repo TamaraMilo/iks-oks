2d2fc8dfa9cc290f577edb6843f0dbbcf815ec27
TamaraMilo <tamara.m.milovanovic@gmail.com>
2022-09-26 11:19:20 +0200

    created state

--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,26 @@
+use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::Addr;
+use cosmwasm_std::{Addr, Storage};
 use cw_storage_plus::Item;
+
+pub static CONFIG_KEY: &[u8] = b"config";
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 pub struct State {
-    pub count: i32,
-    pub owner: Addr,
+    pub player1: Addr,
+    pub player2: Addr,
+    pub contract_addr: Addr,
+    pub mat: Vec<Vec<String>>,
 }
 
 pub const STATE: Item<State> = Item::new("state");
+
+pub fn config(storage: &mut dyn Storage) -> Singleton<State> {
+    singleton(storage, CONFIG_KEY)
+}
+
+pub fn config_read(storage: &dyn Storage) -> ReadonlySingleton<State> {
+    singleton_read(storage, CONFIG_KEY)
+}
