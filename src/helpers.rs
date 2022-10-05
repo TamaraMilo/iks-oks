a4d9252360c8463d20bae83b5ed446607c0cd80d
TamaraMilo <tamara.m.milovanovic@gmail.com>
2022-10-05 11:42:00 +0200

    Formated documents

--- src/helpers.rs
+++ src/helpers.rs
@@ -3,48 +3,43 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
-pub enum GameState 
-{
-    GameWon{player: Addr},
+pub enum GameState {
+    GameWon { player: Addr },
     Tie,
-    InProgess
+    InProgess,
 }
-#[derive(Serialize,Deserialize,Clone, Debug,PartialEq, Eq,JsonSchema)]
-pub struct Coordinates{
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct Coordinates {
     pub x: u8,
     pub y: u8,
 }
-impl Coordinates
-{
-    pub fn new(x: u8, y: u8)->Self
-    {
-        Self { x, y}
+impl Coordinates {
+    pub fn new(x: u8, y: u8) -> Self {
+        Self { x, y }
+    }
+    pub fn index(&self) -> usize {
+        (self.x * 3 + self.y).into()
     }
 }
-#[derive(Serialize,Deserialize,Clone, Debug,PartialEq, Eq,JsonSchema)]
-pub struct Cell
-{
-    pub coordinats: Coordinates,
-    pub player:Option<Addr>,
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct Cell {
+    pub coordinates: Coordinates,
+    pub player: Option<Addr>,
     pub sign: String,
-
 }
-#[derive(Serialize,Deserialize,Clone, Debug,PartialEq, Eq,JsonSchema)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 pub struct Board {
-    pub cells: Vec<Cell>
+    pub cells: Vec<Cell>,
 }
-impl Board{
-    pub fn new() -> Self
-    {
+impl Board {
+    pub fn new() -> Self {
         let mut cells = Vec::new();
 
-        for i in 0..3
-        {
-            for j in 0..3
-            {
-                let coordinats  = Coordinates{ x: i, y: j };
-                let cell = Cell{
-                    coordinats,
+        for i in 0..3 {
+            for j in 0..3 {
+                let coordinates = Coordinates { x: i, y: j };
+                let cell = Cell {
+                    coordinates,
                     player: None,
                     sign: String::from(""),
                 };
@@ -54,22 +49,17 @@ impl Board{
 
         Self { cells }
     }
-    pub fn occupy_cell(&mut self, player: Addr, coordinats: Coordinates, sign: String) -> bool
-    {
-
-        let cell_index: usize = (coordinats.x*3+coordinats.y).into();
-        if self.cells[cell_index].player == None
-        {
+    pub fn occupy_cell(&mut self, player: Addr, coordinates: Coordinates, sign: String) -> bool {
+        let cell_index: usize = coordinates.index();
+        if self.cells[cell_index].player == None {
             self.cells[cell_index].player = Some(player);
             self.cells[cell_index].sign = sign;
             return true;
         }
-        
+
         false
-        
     }
-    pub fn check_for_win(&self, coordinats: Coordinates) -> bool
-    {
+    pub fn check_for_win(&self, coordinates: Coordinates) -> bool {
         let win_lines = vec![
             vec![[1, 2], [4, 8], [3, 6]],
             vec![[0, 2], [4, 7]],
@@ -79,76 +69,39 @@ impl Board{
             vec![[3, 4], [2, 8]],
             vec![[7, 8], [2, 4], [0, 3]],
             vec![[6, 8], [1, 4]],
-            vec![[6, 7], [0, 4], [2, 5]]
+            vec![[6, 7], [0, 4], [2, 5]],
         ];
-        let last_move: usize = (coordinats.x*3+coordinats.y).into();
+        let last_move: usize = coordinates.index();
         let player = self.cells[last_move].player.clone();
-        for i in 0..win_lines[last_move].len()
-        {
+        for i in 0..win_lines[last_move].len() {
             let line = win_lines[last_move][i];
-            if player ==  self.cells[line[0]].player && player ==  self.cells[line[1]].player
-            {
-                return  true;
+            if player == self.cells[line[0]].player && player == self.cells[line[1]].player {
+                return true;
             }
         }
         false
-
     }
-    pub fn draw_board(&self) -> String
-    {
-        let mut board_look = String::from("---------\x20"); 
-        for i in 0..3
-        {
+    pub fn draw_board(&self) -> String {
+        let mut board_look = String::from(
+            "---------
+        ",
+        );
+        for i in 0..3 {
             board_look += &"||".to_string();
-            for j in 0..3
-            {   
-                let cell_index: usize = i*3+j;
+            for j in 0..3 {
+                let cell_index: usize = i * 3 + j;
                 board_look += " ";
-                board_look +=&self.cells[cell_index].sign;
+                board_look += &self.cells[cell_index].sign;
                 board_look += " |";
-                
             }
             board_look += &"|".to_string();
-            board_look += "---------\x20"
+            board_look += "---------
+            "
         }
-
-
 
         board_look
     }
-   
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // use schemars::JsonSchema;
 // use serde::{Deserialize, Serialize};
--- src/msg.rs
+++ src/msg.rs
@@ -1,6 +1,5 @@
-
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use cosmwasm_std::{Addr};
+use cosmwasm_std::Addr;
 
 use crate::helpers::Coordinates;
 
@@ -8,38 +7,35 @@ use crate::helpers::Coordinates;
 pub struct InitMsg {
     pub player1: Addr,
     pub player2: Addr,
-
 }
 #[cw_serde(Serialize)]
 pub enum HandleMsg {
-   PlayMove{ coordinates: Coordinates}
+    PlayMove { coordinates: Coordinates },
 }
 
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
-
     #[returns(TableStatusResponse)]
-    TableStatus{},
+    TableStatus {},
     #[returns(PlayerTurnResponse)]
-    PlayerTurn{},
-    #[returns(GameStatusRespons)]
-    GameStatus{}
-
+    PlayerTurn {},
+    #[returns(GameStatusResponse)]
+    GameStatus {},
 }
 
 // We define a custom struct for each query response
 
 #[cw_serde(Serialize)]
 pub struct TableStatusResponse {
-   pub status: String
+    pub status: String,
 }
 
 #[cw_serde(Serialize)]
-pub struct PlayerTurnResponse{
-    pub turn: Addr
+pub struct PlayerTurnResponse {
+    pub turn: Addr,
 }
 #[cw_serde(Serialize)]
-pub struct GameStatusRespons{
-    pub status: String
+pub struct GameStatusResponse {
+    pub status: String,
 }
